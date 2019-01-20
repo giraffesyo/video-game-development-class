0303ae7f38f7beac068db56d03030e50cb7c090b
Michael McQuade <giraffesyo@gmail.com>
2019-01-19 23:55:44 -0600

    Add movement

---
+++ homework/Ind_McQuade_Michael/Assets/playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // set a default, editable, speed
+    public float speed = 10.0f;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // get the translation for horizontal input (comes from unity input manager)
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        // Add force to our rigidbody
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+
+    }
+}
