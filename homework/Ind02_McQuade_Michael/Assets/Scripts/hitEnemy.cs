63fbe8a3331fff8c09a739a59da05421c44a2530
Michael McQuade <giraffesyo@gmail.com>
2019-01-31 21:41:50 -0600

    add bowser

--- homework/Ind02_McQuade_Michael/Assets/Scripts/hitEnemy.cs
+++ homework/Ind02_McQuade_Michael/Assets/Scripts/hitEnemy.cs
@@ -6,7 +6,6 @@ public class hitEnemy : MonoBehaviour
 {
     private AudioSource audioSource;
     private Animator animator;
-    private int hitCount;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +18,7 @@ public class hitEnemy : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // check if the gameobject we hit has the tag bullet
-        if (collision.gameObject.tag == "bullet" && hitCount < 1 )
+        if (collision.gameObject.tag == "bullet" )
         {
             Debug.Log("goomba was hit");
             // play the hit noise
@@ -27,8 +26,7 @@ public class hitEnemy : MonoBehaviour
             // play the enemy hit animation
             animator.SetTrigger("enemy_hit");
         }
-        // if we already hit something (including the ground), we wont play the noise or trigger the animation
-        hitCount++;
+
     }
 
 }
