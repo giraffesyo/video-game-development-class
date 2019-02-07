304e83d4a73e0983d2f1d734b9af686334c27eb6
Michael McQuade <giraffesyo@gmail.com>
2019-02-07 04:12:38 -0600

    Make enemies go back underground after a short time

--- homework/Ind03_McQuade_Michael/Assets/Scripts/riseAboveGround.cs
+++ homework/Ind03_McQuade_Michael/Assets/Scripts/riseAboveGround.cs
@@ -7,22 +7,33 @@ public class riseAboveGround : MonoBehaviour
 
     // this value is the y point we will raise the character up to
     // should be found heuristically
-    public float riseToY = -1.25f;
+    public float moveToY = -1.25f;
     private Vector3 goalTransform;
+    private bool rising = true;
     //we want them to rise completely in a third of their alive time
     private float riseTime = childrenAppearRandomly.timeAlive / 3.0f;
+    private float timePassed = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        goalTransform = new Vector3(transform.position.x, riseToY, transform.position.z);
+        goalTransform = new Vector3(transform.position.x, moveToY, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
+        timePassed += Time.deltaTime;
+        // if we've passed 2/3 of the time, go back down
+        if(rising && timePassed > riseTime * 2)
+        {
+            goalTransform = new Vector3(transform.position.x, -2.0f, transform.position.z);
+            rising = false;
+
+        }
 
         // set a new position based on our goal position and how much time has passed 
         transform.position = Vector3.Lerp(transform.position, goalTransform, riseTime * Time.deltaTime);
+
     }
 }
