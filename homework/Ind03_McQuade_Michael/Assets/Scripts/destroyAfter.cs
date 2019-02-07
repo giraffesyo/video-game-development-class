07a448ae7864a3e45ce43132a618a14215bd4f39
Michael McQuade <giraffesyo@gmail.com>
2019-02-07 05:42:05 -0600

    Destroy koopas after a bit

---
+++ homework/Ind03_McQuade_Michael/Assets/Scripts/destroyAfter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyAfter : MonoBehaviour
+{
+
+    private float timeSinceSpawned = 0.0f;
+    public float aliveForSeconds = 1.0f;
+    private int renderedFrames = 0;
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceSpawned += Time.deltaTime;
+        if (timeSinceSpawned > aliveForSeconds)
+        {
+            Destroy(this.gameObject);
+        }
+
+        // blink every 5 frames
+        if(renderedFrames == 5)
+        {
+            renderedFrames = 0;
+            // flip rendered
+            rend.enabled = !rend.enabled;
+        }
+        renderedFrames++;
+    }
+}
--- homework/Ind03_McQuade_Michael/Assets/Scripts/hitEnemy.cs
+++ homework/Ind03_McQuade_Michael/Assets/Scripts/hitEnemy.cs
@@ -6,26 +6,35 @@ public class hitEnemy : MonoBehaviour
 {
     private AudioSource audioSource;
     private Animator animator;
+    private int hitCount = 0;
+    private GameObject objectHit;
 
     // Start is called before the first frame update
     void Start()
     {
         // get the audiosource and animator off the instance
         audioSource = GetComponent<AudioSource>();
-        animator = GetComponent<Animator>();
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // check if the gameobject we hit has the tag bullet
-        if (collision.gameObject.tag == "bullet" )
+        // check if the gameobject we hit has the tag enemy
+        if (collision.gameObject.tag == "enemy" && hitCount == 0)
         {
-            Debug.Log("goomba was hit");
+            // store the gameobject that we hit
+            objectHit = collision.gameObject;
+            //store the animator of that object
+            animator = objectHit.GetComponent<Animator>();
             // play the hit noise
             audioSource.Play();
             // play the enemy hit animation
             animator.SetTrigger("enemy_hit");
         }
+        // increase hitcount (we hit something after all!)
+        hitCount++;
+        //now that we've hit something, destroy the koopa (they're single use)
+        this.gameObject.AddComponent<destroyAfter>();
 
     }
 
