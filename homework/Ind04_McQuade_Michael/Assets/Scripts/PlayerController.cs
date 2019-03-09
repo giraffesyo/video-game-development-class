c51f9c9c1c3ef694c0db9d69fc9738f7107d4051
Michael McQuade <github@giraffesyo.io>
2019-03-09 04:31:30 -0600

    Add winning
    Can now win after collecting all the pick ups

--- homework/Ind04_McQuade_Michael/Assets/Scripts/PlayerController.cs
+++ homework/Ind04_McQuade_Michael/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    public Text countText;
+    public Text winText;
+
     public float speed = 10.0f;
+    private int count;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        UpdateCountText();
+        winText.text = "";
     }
     void FixedUpdate()
     {
@@ -26,6 +34,17 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count += 1;
+            UpdateCountText();
+        }
+    }
+
+    void UpdateCountText()
+    {
+        countText.text = $"Count: {count}";
+        if(count >= 12)
+        {
+            winText.text = "You win!";
         }
     }
 }
