02ceca9e2c4127e2fcca95dadce20eebf8e79778
Michael McQuade <github@giraffesyo.io>
2019-03-08 21:32:14 -0600

    Pick ups, player, and background added

---
+++ homework/Ind04_McQuade_Michael/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    
+    private void Update()
+    {
+        // Multiplying by Time.deltaTime to ensure that the rotation is smooth
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);    
+    }
+}
