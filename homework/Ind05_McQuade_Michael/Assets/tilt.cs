6038a9945a2964df00f1b4fdc839d99f1e2bb40e
Michael McQuade <giraffesyo@gmail.com>
2019-05-02 00:37:59 -0500

    First ball moves through maze

---
+++ homework/Ind05_McQuade_Michael/Assets/tilt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tilt : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 50;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 Velocity = new Vector3(-vertical, 0, horizontal);
+        Quaternion deltaRotation = Quaternion.Euler(Velocity * Time.deltaTime * speed);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+}
