c7f0aa7d00de4c5153e223b8365dfd6e4f826724
Michael McQuade <giraffesyo@gmail.com>
2019-05-02 02:16:03 -0500

    Add start menu

---
+++ homework/Ind05_McQuade_Michael/Assets/Menu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public bool paused = true;
+    public Button QuitButton;
+    public Button PlayButton;
+    public GameObject MenuContainer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuitButton.onClick.AddListener(QuitClicked);
+        PlayButton.onClick.AddListener(PlayClicked);
+        Pause();
+    }
+
+
+
+    public void PlayClicked() {
+        Unpause();
+    }
+
+    public void QuitClicked() {
+
+        // exit the game
+        Application.Quit();
+}
+
+
+    private void hideMenu() {
+        MenuContainer.SetActive(false);
+    }
+
+    private void showMenu()
+    {
+        MenuContainer.SetActive(true);
+    }
+
+    private void Pause()
+    {
+        showMenu();
+        paused = true;
+        Time.timeScale = 0;
+    }
+
+    private void Unpause() 
+    {
+        hideMenu();
+        paused = false;
+        Time.timeScale = 1;
+    }
+
+}
---
+++ homework/Ind05_McQuade_Michael/acknolwedgements.md
@@ -0,0 +1,9 @@
+Acknowledgements:
+
+The background image was provided for free by Unsplash user Omar Sotillo Franco @omarsotillo, downloaded from: https://unsplash.com/photos/ZgicbyFH7sw
+
+Button backgrounds were created using Da Button Factory at https://dabuttonfactory.com
+
+Ki Comic font free for personal use from https://www.dafont.com/ki-comic.font
+
+
