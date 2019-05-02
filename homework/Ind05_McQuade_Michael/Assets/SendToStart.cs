3ad56a11c3156d99346a942df735e8c4881f02b1
Michael McQuade <giraffesyo@gmail.com>
2019-05-02 01:01:26 -0500

    Create pitfalls

---
+++ homework/Ind05_McQuade_Michael/Assets/SendToStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendToStart : MonoBehaviour
+{
+    public GameObject startingPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.MovePosition(startingPoint.transform.position);
+        }
+    }
+}
